45d6ef33682eec09895b2ef906014e9980999159
light <apochmn@naver.com>
2023-08-28 23:19:06 +0900

    #14 feat: 이미지 삭제 (#20)
    * #14 feat : 이미지 삭제 dto 생성 및 dto 요청/응답 패키지 분리
    
    * #14 feat : 이미지 삭제 api 구현
    
    * #14 test : 이미지 삭제 테스트 코드

--- secondhand/src/test/java/com/codesquad/secondhand/domain/image/controller/ImageControllerTest.java
+++ secondhand/src/test/java/com/codesquad/secondhand/domain/image/controller/ImageControllerTest.java
@@ -9,11 +9,13 @@ import java.nio.file.Paths;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.codesquad.secondhand.annotation.IntegrationTest;
+import com.codesquad.secondhand.domain.image.dto.request.ImageDeleteRequestDto;
 import com.codesquad.secondhand.domain.image.service.ImageService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -46,5 +48,25 @@ class ImageControllerTest {
 		mockMvc.perform(MockMvcRequestBuilders.multipart("/api/images")
 				.file(mockMultipartFile))
 			.andExpect(status().isOk());
+	}
+
+	@Test
+	@DisplayName("이미지 id 를 입력받아 해당 이미지를 삭제한다.")
+	void deleteImage() throws Exception {
+		//given
+		Path path = Paths.get("src/test/resources/test-image.jpg");
+		String parameterName = "file";
+		String originalFileName = "test-image.jpg";
+		String contentType = "image/jpeg";
+		byte[] content = Files.readAllBytes(path);
+
+		MockMultipartFile mockMultipartFile = new MockMultipartFile(parameterName, originalFileName, contentType,
+			content);
+
+		ImageDeleteRequestDto requestDto = new ImageDeleteRequestDto(imageService.uploadImg(mockMultipartFile).getId());
+		String request = objectMapper.writeValueAsString(requestDto);
+
+		// when & then
+		mockMvc.perform(MockMvcRequestBuilders.delete("/api/images").contentType(MediaType.APPLICATION_JSON).content(request)).andExpect(status().isOk());
 	}
 }
