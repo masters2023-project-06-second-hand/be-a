6cbb2fac7b824255fbd3d809a0738e29aed999df
Charlie <kdjin1215@Naver.com>
2023-08-31 17:54:03 +0900

    #32 Chore: yml 파일 암호화 (#33)
    * #32 chore: jasypt 의존성 추가
    
    * #32 chore: jasypt 를 사용하여 yml 파일 암호화

--- secondhand/src/main/java/com/codesquad/secondhand/domain/member/dto/request/SignupRequest.java
+++ secondhand/src/main/java/com/codesquad/secondhand/domain/member/dto/request/SignupRequest.java
@@ -9,15 +9,23 @@ import com.codesquad.secondhand.domain.member.entity.Member;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class SignupRequest {
 	@NotBlank(message = "이름을 입력해 주세요.")
 	private String nickname;
 	private String profileImg;
 	@NotNull(message = "필수로 하나의 지역은 선택 해야 합니다.")
 	private List<Long> regionsId;
+
+	@Builder
+	public SignupRequest(String nickname, String profileImg, List<Long> regionsId) {
+		this.nickname = nickname;
+		this.profileImg = profileImg;
+		this.regionsId = regionsId;
+	}
 
 	public Member toEntity(String email) {
 		return Member.builder()
