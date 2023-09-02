a5b427c4526ed419b90810140aeb53be3604b40a
CDBchan <kdjin1215@naver.com>
2023-09-03 00:52:10 +0900

    #42 test: 관심 상품들의 카테고리 목록 조회 테스트 작성

--- secondhand/src/test/java/com/codesquad/secondhand/domain/reaction/controller/ReactionControllerTest.java
+++ secondhand/src/test/java/com/codesquad/secondhand/domain/reaction/controller/ReactionControllerTest.java
@@ -4,9 +4,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.Arrays;
 
+import org.assertj.core.api.Assertions;
+import org.json.JSONArray;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.codesquad.secondhand.BaseControllerTest;
@@ -79,4 +82,53 @@ class ReactionControllerTest extends BaseControllerTest {
 	private void saveDummyImage(String image) {
 		imageJpaRepository.save(Image.builder().imgUrl(image).build());
 	}
+
+	@Test
+	@DisplayName("사용자가 좋아요를 누른 상품들의 카테고리 목록을 응답에 전송한다.")
+	void findAllCategoryOfReactedProducts() throws Exception {
+		//1. 이미지 저장
+		saveDummyImage("imageTest1");
+		saveDummyImage("imageTest2");
+		saveDummyImage("imageTest3");
+		saveDummyImage("imageTest4");
+
+		//2.1 첫번째 상품 저장
+		ProductSaveAndUpdateRequest productSaveAndUpdateRequest = new ProductSaveAndUpdateRequest("상품명", 1L, 100000L,
+			"상품내용", 1L,
+			Arrays.asList(1L, 2L));
+		Long productId = productService.save(productSaveAndUpdateRequest, MEMBER_ID);
+
+		//2.2 두번째 상품 저장
+		ProductSaveAndUpdateRequest secondProductSaveAndUpdateRequest = new ProductSaveAndUpdateRequest("상품명", 2L,
+			100000L,
+			"상품내용", 1L,
+			Arrays.asList(3L, 4L));
+		Long secondProductId = productService.save(secondProductSaveAndUpdateRequest, MEMBER_ID);
+
+		//3.1 첫번째 상품 반응 등록
+		ReactionUpdateRequest react = new ReactionUpdateRequest(true);
+		reactionService.update(productId, MEMBER_ID, react);
+
+		//3.2 두번째 상품 반응 등록
+		reactionService.update(secondProductId, MEMBER_ID, react);
+
+		//4. 좋아요를 누른 카테고리 목록 조회
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.get("/api/members/{memberId}/likes/categories", MEMBER_ID)
+					.header(AUTHORIZATION, JWT_TOKEN_PREFIX + jwt.getAccessToken())
+					.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$[0].id").exists())
+			.andExpect(jsonPath("$[0].name").exists())
+			.andExpect(jsonPath("$[0].imgUrl").doesNotExist())
+			.andExpect(jsonPath("$[1].id").exists())
+			.andExpect(jsonPath("$[1].name").exists())
+			.andExpect(jsonPath("$[1].imgUrl").doesNotExist())
+			.andReturn();
+
+		String responseBody = result.getResponse().getContentAsString();
+		JSONArray jsonArray = new JSONArray(responseBody);
+		Assertions.assertThat(jsonArray.length()).isEqualTo(2);
+	}
+
 }
