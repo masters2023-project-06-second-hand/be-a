180216a5f641e9d3feca8c4b05f96f73261aa231
CDBchan <kdjin1215@naver.com>
2023-09-04 17:06:17 +0900

    #41 test: 상품 판매내역조회 테스트 작성

--- secondhand/src/test/java/com/codesquad/secondhand/domain/product/controller/ProductControllerTest.java
+++ secondhand/src/test/java/com/codesquad/secondhand/domain/product/controller/ProductControllerTest.java
@@ -226,4 +226,156 @@ class ProductControllerTest extends BaseControllerTest {
 		JSONArray jsonArray = new JSONArray(responseBody);
 		Assertions.assertThat(jsonArray.length()).isEqualTo(2);
 	}
+
+	@Test
+	@DisplayName("상품 판매 내역 조회 요청시 statusId = 0 또는 1 이면 특정 회원이 판매중 또는 예약중인 상품만을 응답에 포함한다.")
+	void findSalesProductsWithSaleOrReservedStatus() throws Exception {
+		// Given
+
+		//1. 이미지 저장
+		saveDummyImage("imageTest1");
+		saveDummyImage("imageTest2");
+
+		//2. 상품 저장
+		ProductSaveAndUpdateRequest productRequestCat1Reg1 = new ProductSaveAndUpdateRequest("상품명", 1L, 100000L, "상품내용",
+			1L, Arrays.asList(1L, 2L));
+		Long productId = productService.save(productRequestCat1Reg1, MEMBER_ID);
+
+		ProductSaveAndUpdateRequest productRequestCat2Reg1 = new ProductSaveAndUpdateRequest("상품명", 2L, 100000L, "상품내용",
+			1L, Arrays.asList(1L, 2L));
+		productService.save(productRequestCat2Reg1, MEMBER_ID);
+
+		ProductSaveAndUpdateRequest productRequestCat1Reg2 = new ProductSaveAndUpdateRequest("상품명", 1L, 100000L, "상품내용",
+			2L, Arrays.asList(1L, 2L));
+		productService.save(productRequestCat1Reg2, MEMBER_ID);
+
+		//2.1 위와다른 멤버가 작성한 상품 등록
+		ProductSaveAndUpdateRequest productRequestWithDifferentMember = new ProductSaveAndUpdateRequest("상품명", 1L,
+			100000L, "상품내용",
+			2L, Arrays.asList(1L, 2L));
+		productService.save(productRequestWithDifferentMember, 2L);
+
+		//3. 상품 상태 수정 (productId = 1 인 상품의 status 를 판매 완료로 바꾼다.)
+		ProductUpdateRequest productUpdateRequest = new ProductUpdateRequest("판매완료");
+		productService.updateStatus(productId, productUpdateRequest);
+
+		//when & then
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/members/{memberId}/sales", MEMBER_ID)
+				.header(AUTHORIZATION, JWT_TOKEN_PREFIX + jwt.getAccessToken())
+				.param("statusId", String.valueOf(0))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$[0].id").exists())
+			.andExpect(jsonPath("$[0].writerId").exists())
+			.andExpect(jsonPath("$[0].thumbnailUrl").exists())
+			.andExpect(jsonPath("$[0].name").exists())
+			.andExpect(jsonPath("$[0].region").exists())
+			.andExpect(jsonPath("$[0].createdAt").exists())
+			.andExpect(jsonPath("$[0].status").exists())
+			.andExpect(jsonPath("$[0].price").exists())
+			.andExpect(jsonPath("$[0].likeCount").exists())
+			.andExpect(jsonPath("$[0].chattingCount").exists())
+			.andReturn();
+
+		String responseBody = result.getResponse().getContentAsString();
+		JSONArray jsonArray = new JSONArray(responseBody);
+		Assertions.assertThat(jsonArray.length()).isEqualTo(2);
+	}
+
+	@Test
+	@DisplayName("상품 판매 내역 조회 요청시 statusId = null 이면 특정 회원이 상품등록을 한 모든 상품을 응답에 포함한다.")
+	void findSalesProductsWithNoStatusId() throws Exception {
+		// Given
+
+		//1. 이미지 저장
+		saveDummyImage("imageTest1");
+		saveDummyImage("imageTest2");
+
+		//2. 상품 저장
+		ProductSaveAndUpdateRequest productRequestCat1Reg1 = new ProductSaveAndUpdateRequest("상품명", 1L, 100000L, "상품내용",
+			1L, Arrays.asList(1L, 2L));
+		Long productId = productService.save(productRequestCat1Reg1, MEMBER_ID);
+
+		ProductSaveAndUpdateRequest productRequestCat2Reg1 = new ProductSaveAndUpdateRequest("상품명", 2L, 100000L, "상품내용",
+			1L, Arrays.asList(1L, 2L));
+		productService.save(productRequestCat2Reg1, MEMBER_ID);
+
+		ProductSaveAndUpdateRequest productRequestCat1Reg2 = new ProductSaveAndUpdateRequest("상품명", 1L, 100000L, "상품내용",
+			2L, Arrays.asList(1L, 2L));
+		productService.save(productRequestCat1Reg2, MEMBER_ID);
+
+		//3. 상품 상태 수정 (productId = 1 인 상품의 status 를 판매 완료로 바꾼다.)
+		ProductUpdateRequest productUpdateRequest = new ProductUpdateRequest("판매완료");
+		productService.updateStatus(productId, productUpdateRequest);
+
+		//when & then
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/members/{memberId}/sales", MEMBER_ID)
+				.header(AUTHORIZATION, JWT_TOKEN_PREFIX + jwt.getAccessToken())
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$[0].id").exists())
+			.andExpect(jsonPath("$[0].writerId").exists())
+			.andExpect(jsonPath("$[0].thumbnailUrl").exists())
+			.andExpect(jsonPath("$[0].name").exists())
+			.andExpect(jsonPath("$[0].region").exists())
+			.andExpect(jsonPath("$[0].createdAt").exists())
+			.andExpect(jsonPath("$[0].status").exists())
+			.andExpect(jsonPath("$[0].price").exists())
+			.andExpect(jsonPath("$[0].likeCount").exists())
+			.andExpect(jsonPath("$[0].chattingCount").exists())
+			.andReturn();
+
+		String responseBody = result.getResponse().getContentAsString();
+		JSONArray jsonArray = new JSONArray(responseBody);
+		Assertions.assertThat(jsonArray.length()).isEqualTo(3);
+	}
+
+	@Test
+	@DisplayName("상품 판매 내역 조회 요청시 statusId = 2 이면 특정 회원이 등록한 상품중 상태가 판매완료인 상품을 응답에 포함한다.")
+	void findSalesProductsWithSoldStatusId() throws Exception {
+		// Given
+
+		//1. 이미지 저장
+		saveDummyImage("imageTest1");
+		saveDummyImage("imageTest2");
+
+		//2. 상품 저장
+		ProductSaveAndUpdateRequest productRequestCat1Reg1 = new ProductSaveAndUpdateRequest("상품명", 1L, 100000L, "상품내용",
+			1L, Arrays.asList(1L, 2L));
+		Long productId = productService.save(productRequestCat1Reg1, MEMBER_ID);
+
+		ProductSaveAndUpdateRequest productRequestCat2Reg1 = new ProductSaveAndUpdateRequest("상품명", 2L, 100000L, "상품내용",
+			1L, Arrays.asList(1L, 2L));
+		productService.save(productRequestCat2Reg1, MEMBER_ID);
+
+		ProductSaveAndUpdateRequest productRequestCat1Reg2 = new ProductSaveAndUpdateRequest("상품명", 1L, 100000L, "상품내용",
+			2L, Arrays.asList(1L, 2L));
+		productService.save(productRequestCat1Reg2, MEMBER_ID);
+
+		//3. 상품 상태 수정 (productId = 1 인 상품의 status 를 판매 완료로 바꾼다.)
+		ProductUpdateRequest productUpdateRequest = new ProductUpdateRequest("판매완료");
+		productService.updateStatus(productId, productUpdateRequest);
+
+		//when & then
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/members/{memberId}/sales", MEMBER_ID)
+				.param("statusId", String.valueOf(2))
+				.header(AUTHORIZATION, JWT_TOKEN_PREFIX + jwt.getAccessToken())
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$[0].id").exists())
+			.andExpect(jsonPath("$[0].writerId").exists())
+			.andExpect(jsonPath("$[0].thumbnailUrl").exists())
+			.andExpect(jsonPath("$[0].name").exists())
+			.andExpect(jsonPath("$[0].region").exists())
+			.andExpect(jsonPath("$[0].createdAt").exists())
+			.andExpect(jsonPath("$[0].status").exists())
+			.andExpect(jsonPath("$[0].price").exists())
+			.andExpect(jsonPath("$[0].likeCount").exists())
+			.andExpect(jsonPath("$[0].chattingCount").exists())
+			.andReturn();
+
+		String responseBody = result.getResponse().getContentAsString();
+		JSONArray jsonArray = new JSONArray(responseBody);
+		Assertions.assertThat(jsonArray.length()).isEqualTo(1);
+	}
 }
