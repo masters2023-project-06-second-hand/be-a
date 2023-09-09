c49ea8e05e6e4430f6f3454f0ac9f2e5c6deae62
light <apochmn@naver.com>
2023-09-09 18:31:33 +0900

    #67 test: 멤버 service 통합테스트 (#81)
    * #67 refactor : MemberService signOut 메서드 delete 누락 수정
    
    * #67 refactor : MemberService 통합 테스트 작성

---
+++ secondhand/src/test/java/com/codesquad/secondhand/domain/member/service/MemberQueryServiceTest.java
@@ -0,0 +1,77 @@
+package com.codesquad.secondhand.domain.member.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.codesquad.secondhand.annotation.ServiceIntegrationTest;
+import com.codesquad.secondhand.domain.member.entity.Member;
+import com.codesquad.secondhand.exception.CustomRuntimeException;
+
+@ServiceIntegrationTest
+class MemberQueryServiceTest {
+
+	@Autowired
+	MemberQueryService memberQueryService;
+
+	@DisplayName(" id로 회원을 찾을 수 있다.")
+	@Test
+	void findByIdSuccess() {
+		// given
+		Long memberId = 1L;
+		// when
+		Member actual = memberQueryService.findById(memberId);
+		// then
+		assertThat(actual.getId()).isEqualTo(memberId);
+
+	}
+
+	@DisplayName("존재하지 않는 회원 id를 입력하면 예외가 발생한다.")
+	@Test
+	void findByIdFailed() {
+		// given
+		Long memberId = 155L;
+		// when&then
+		assertThatThrownBy(() -> memberQueryService.findById(memberId)).isInstanceOf(CustomRuntimeException.class);
+	}
+
+	@DisplayName("회원을 저장 한다.")
+	@Test
+	void signup() {
+		// given
+		Member member = Member.builder()
+			.email("test@email.com")
+			.nickname("service")
+			.selectedRegion(1L)
+			.profileImg("test")
+			.build();
+		// when
+		memberQueryService.save(member);
+		Member savedMember = memberQueryService.findById(member.getId());
+		// then
+		assertThat(savedMember.getEmail()).isEqualTo(member.getEmail());
+		assertThat(savedMember.getNickname()).isEqualTo(member.getNickname());
+		assertThat(savedMember.getSelectedRegion()).isEqualTo(member.getSelectedRegion());
+		assertThat(savedMember.getProfileImg()).isEqualTo(member.getProfileImg());
+	}
+
+	@DisplayName("이메일로 member 객체를 찾아온다")
+	@Test
+	void findByEmail() {
+		// given
+		Long memberId = 1L;
+		Member member = memberQueryService.findById(memberId);
+		String email = member.getEmail();
+
+		// when
+		Member findMember = memberQueryService.findByEmail(email).get();
+
+		// then
+		assertThat(findMember.getId()).isEqualTo(member.getId());
+		assertThat(findMember.getEmail()).isEqualTo(member.getEmail());
+		assertThat(findMember.getSelectedRegion()).isEqualTo(member.getSelectedRegion());
+	}
+}
+
---
+++ secondhand/src/test/java/com/codesquad/secondhand/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,79 @@
+package com.codesquad.secondhand.domain.member.service;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.codesquad.secondhand.annotation.ServiceIntegrationTest;
+import com.codesquad.secondhand.domain.member.dto.request.SignupRequest;
+import com.codesquad.secondhand.domain.member.entity.Member;
+import com.codesquad.secondhand.domain.member_region.entity.MemberRegion;
+import com.codesquad.secondhand.domain.member_region.service.MemberRegionQueryService;
+import com.codesquad.secondhand.domain.token.repository.TokenJpaRepository;
+import com.codesquad.secondhand.exception.CustomRuntimeException;
+
+@ServiceIntegrationTest
+class MemberServiceTest {
+
+	@Autowired
+	MemberService memberService;
+
+	@Autowired
+	MemberQueryService memberQueryService;
+
+	@Autowired
+	MemberRegionQueryService memberRegionQueryService;
+
+	@Autowired
+	RedisTemplate<String, Object> redisBlackListTemplate;
+
+	@Autowired
+	TokenJpaRepository tokenJpaRepository;
+
+
+
+	@AfterEach
+	void resetBlackList() {
+		redisBlackListTemplate.delete("eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJtZW1iZXJJZCI6MSwiY3JlYXRlZE1pbGxpcyI6MTY5Mzc5NjE4NTE0NiwiZXhwIjoxNzI1MzMyMTg1fQ.QKKVe4mEOOO8hzVsQ5xyDghyQluxhOAWyNfV3vIJSfI");
+	}
+
+	@DisplayName("회원가입에 성공한다.")
+	@Test
+	void signUpSuccess() {
+		// given
+		List<Long> ids = List.of(1L,2L);
+		SignupRequest signupRequest = new SignupRequest("test","test", ids);
+		String requestEmail = "test@email.com";
+		// when
+		memberService.signUp(signupRequest,requestEmail);
+		Member savedMember = memberQueryService.findByEmail(requestEmail).get();
+		List<MemberRegion> memberRegions = memberRegionQueryService.findAllMemberRegion(savedMember.getId());
+		// then
+		assertThat(savedMember.getEmail()).isEqualTo(requestEmail);
+		assertThat(savedMember.getNickname()).isEqualTo(signupRequest.getNickname());
+		assertThat(savedMember.getProfileImg()).isEqualTo(signupRequest.getProfileImg());
+		assertThat(memberRegions.get(0).getRegion().getId()).isEqualTo(signupRequest.getRegionsId().get(0));
+	}
+
+	@DisplayName("중복된 닉네임 입력시 회원가입에 실패한다 ")
+	@Test
+	void signUpFailed() {
+		// given
+		List<Long> ids = List.of(1L,2L);
+		SignupRequest signupRequest = new SignupRequest("test","test", ids);
+		String requestEmail = "test@email.com";
+		memberService.signUp(signupRequest,requestEmail);
+		String savedMemberNickname = memberQueryService.findByEmail(requestEmail).get().getNickname();
+		SignupRequest existNicknameRequest = new SignupRequest(savedMemberNickname,"test", ids);
+		// when & then
+		assertThatThrownBy(() -> memberService.signUp(existNicknameRequest,requestEmail)).isInstanceOf(
+			CustomRuntimeException.class);
+	}
+}
