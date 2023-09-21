819e7804a17b54e35caa3d61d10ad17a6d83a52d
CDBchan <kdjin1215@naver.com>
2023-09-21 11:44:33 +0900

    #131 test: 채팅방 입장 테스트 작성

--- secondhand/src/test/java/com/codesquad/secondhand/domain/chat/controller/ChatControllerTest.java
+++ secondhand/src/test/java/com/codesquad/secondhand/domain/chat/controller/ChatControllerTest.java
@@ -79,6 +79,41 @@ class ChatControllerTest extends BaseControllerTest {
 			.andExpect(jsonPath("$.chatRoomId").value(expected));
 	}
 
+	@Test
+	@DisplayName("채팅방 들어가기 api를 통해 특정 채팅방에 필요한 모든 데이터를 응답에 포함한다.")
+	void getChatDetails() throws Exception {
+		// given
+
+		//1. memberId 가 2인 accessToken 생성
+		Jwt purchaserJwtToken = jwtProvider.createTokens(Map.of("memberId", PURCHASER_ID));
+
+		//2. product 저장 (채팅방을 생성하려면 특정 물품이 있어야함)
+		Long savedProductId = saveProduct();
+
+		//3. ChatRequest 생성
+		ChatRequest chatRequest = ChatRequest.builder()
+			.productId(savedProductId)
+			.sellerId(MEMBER_ID)
+			.build();
+		String requestJson = objectMapper.writeValueAsString(chatRequest);
+
+		//4. chatRoom 생성
+		Long chatRoomId = chatService.getChatRoom(chatRequest, PURCHASER_ID);
+
+		// when & then
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/chats/{chatRoomId}", chatRoomId)
+				.header(AUTHORIZATION, JWT_TOKEN_PREFIX + purchaserJwtToken.getAccessToken())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(requestJson))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.product.name").exists())
+			.andExpect(jsonPath("$.product.price").exists())
+			.andExpect(jsonPath("$.product.thumbnailUrl").exists())
+			.andExpect(jsonPath("$.opponentName").exists())
+			.andExpect(jsonPath("$.chatRoomId").exists())
+			.andExpect(jsonPath("$.messages").isEmpty());
+	}
+
 	private Long saveProduct() {
 		//2. 이미지 저장
 		saveDummyImage("imageTest1");
