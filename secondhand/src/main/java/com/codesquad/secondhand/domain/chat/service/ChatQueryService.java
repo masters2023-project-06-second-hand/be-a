9ce22ae2e12b6b14a453d2f11b5710adc0aabb55
Charlie <kdjin1215@Naver.com>
2023-10-02 15:53:21 +0900

    #146 refactor: 5주차 리뷰 반영 (#147)

--- secondhand/src/main/java/com/codesquad/secondhand/domain/chat/service/ChatQueryService.java
+++ secondhand/src/main/java/com/codesquad/secondhand/domain/chat/service/ChatQueryService.java
@@ -12,6 +12,8 @@ import com.codesquad.secondhand.domain.chat.repository.ChatMessageQueryRepositor
 import com.codesquad.secondhand.domain.chat.repository.ChatRoomJpaRepository;
 import com.codesquad.secondhand.domain.member.entity.Member;
 import com.codesquad.secondhand.domain.product.entity.Product;
+import com.codesquad.secondhand.exception.CustomRuntimeException;
+import com.codesquad.secondhand.exception.errorcode.ChatException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,13 +26,22 @@ public class ChatQueryService {
 	private final ChatMessageJpaRepository chatMessageJpaRepository;
 	private final ChatMessageQueryRepository chatMessageQueryRepository;
 
+	/**
+	 * DB에 특정 상품과 구매자에 대한 ChatRoom 이 이미 생성되어 있다면 찾고, 없다면 ChatRoom 을 만들어주는 메서드 이다.
+	 *
+	 * @param participantId
+	 * @param product
+	 * @param participant
+	 * @return
+	 */
 	public ChatRoom findOrSaveChatRoom(Long participantId, Product product, Member participant) {
 		return chatRoomJpaRepository.findByMemberIdAndProductId(participantId, product.getId())
 			.orElseGet(() -> chatRoomJpaRepository.save(ChatRoom.of(product, participant)));
 	}
 
 	public ChatRoom findChatRoomByChatRoomId(Long chatRoomId) {
-		return chatRoomJpaRepository.findById(chatRoomId).orElseThrow();
+		return chatRoomJpaRepository.findById(chatRoomId)
+			.orElseThrow(() -> new CustomRuntimeException(ChatException.INVALID_CHAT_ROOM_ID));
 	}
 
 	public List<ChatMessage> findAllChatMessageByChatRoom(ChatRoom chatRoom) {
@@ -44,15 +55,15 @@ public class ChatQueryService {
 	public List<ChatRoom> findAllChatRoomByMember(Long memberId) {
 		return chatRoomJpaRepository.findAllByMemberId(memberId);
 	}
-  
+
 	public Long getUnReadCount(Member opponent, ChatRoom chatRoom) {
 		return chatMessageJpaRepository.countUnreadMessages(opponent, chatRoom);
 	}
 
 	public ChatMessage findLastMessage(ChatRoom chatRoom) {
 		return chatMessageQueryRepository.findLatestMessage(chatRoom);
-  }
-  
+	}
+
 	public Long countByProduct(Product product) {
 		return chatRoomJpaRepository.countByProduct(product);
 	}
